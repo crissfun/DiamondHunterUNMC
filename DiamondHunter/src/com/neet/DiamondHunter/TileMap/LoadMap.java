339ba404512e2bdabdeb4fc65254e6e7ed06a537
Fun <khcy5fgo@nottingham.edu.my>
2016-12-22 02:25:07 +0800

    Updated UI improvement in whole project version 2.0

---
+++ DiamondHunter/src/com/neet/DiamondHunter/TileMap/LoadMap.java
@@ -0,0 +1,23 @@
+
+package com.neet.DiamondHunter.TileMap;
+
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class LoadMap extends JPanel{
+	
+	
+	private static final long serialVersionUID = 1L;
+	private TileMap tilemap;
+
+	public LoadMap(){			//Load Map and tile
+		tilemap = new TileMap(16);
+		tilemap.loadMap("/Maps/testmap.map");
+		tilemap.loadTiles("/Tilesets/testtileset.gif");	
+	}
+	
+	public void paint (Graphics g){ //Call draw from tilemap
+		tilemap.draw(g);
+	}
+}
